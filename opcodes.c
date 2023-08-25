aa05a4d6fe669821e364457a0951ea314b1ba4ec
El-gibbor <chiagoziem11@yahoo.com>
2023-08-25 17:47:11 +0400

    implement pop

--- opcodes.c
+++ opcodes.c
@@ -1,26 +1,6 @@
 #include "monty.h"
 
 /**
- * create_node - Creates a new node and adds it to the stack.
- * @head: Pointer to the top of the stack.
- * @n: Value to store in the new node.
- *
- * Return: Address of the newly created node, or NULL on failure.
- */
-stack_t *create_node(stack_t **head, int n)
-{
-	stack_t *new_node = malloc(sizeof(stack_t));
-
-	if (!head || !new_node)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
-	return (new_node);
-}
-
-/**
  * push - Pushes an element onto the stack.
  * @stack: Pointer to the top of the stack.
  * @line_number: Current line number in the file.
@@ -31,15 +11,8 @@ void push(stack_t **stack, unsigned int line_number)
 	stack_t *new_node;
 	char *op_arg = strtok(NULL, " \t\n");
 
-	if (op_arg == NULL)
-	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
-		free_memory(stack);
-		exit(EXIT_FAILURE);
-	}
-
 	value = _atoi(op_arg);
-	if (value == 0 && (strcmp(op_arg, "0") != 0 && strcmp(op_arg, "-0") != 0))
+	if (!op_arg || (!value && (strcmp(op_arg, "0") && strcmp(op_arg, "-0"))))
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		free_memory(stack);
@@ -87,3 +60,21 @@ void pint(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", (*stack)->n);
 }
+
+/**
+ * pop - Removes the top element of the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Current line number in the file.
+ */
+void pop(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		free_memory(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	*stack = (*stack)->next;
+}
+
