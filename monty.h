ba7cf40a49acd194d07d2dd023fe059cfd435dd0
softlink2 <121310192+softlink2@users.noreply.github.com>
2023-08-25 18:08:02 +0400

    Update monty.h

--- monty.h
+++ monty.h
@@ -68,7 +68,6 @@ void pop(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 
 /* GLOBAL VARIABLES */
