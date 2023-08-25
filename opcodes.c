48b51446c9fdd2d9662756725f0a64256934dc51
softlink2 <121310192+softlink2@users.noreply.github.com>
2023-08-25 18:07:06 +0400

    Update opcodes.c

--- opcodes.c
+++ opcodes.c
@@ -77,4 +77,23 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	*stack = (*stack)->next;
 }
+#include "monty.h"
 
+/**
+ * swap - Swaps the top two elements of the stack.
+ * @stack: Pointer to the top of the stack.
+ * @line_number: Current line number in the file.
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		free_memory(stack);
+		exit(EXIT_FAILURE);
+	}
+
+	int tmp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = tmp;
+}
