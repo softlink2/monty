76da5c72a5b34dd2e476a611bb4d9fedcea075e2
softlink2 <121310192+softlink2@users.noreply.github.com>
2023-08-25 18:36:50 +0400

    Update opcodes.c

--- opcodes.c
+++ opcodes.c
@@ -88,7 +88,7 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	int tmp;
 
-	if (*stack == NULL || (*stack)->next == NULL)
+	if (*stack != NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		free_memory(stack);
