a67b5920f16b9e4ac57747e22e4c4e130e9b6fa6
softlink2 <121310192+softlink2@users.noreply.github.com>
2023-08-25 12:55:45 +0400

    Update node_ops.c

--- node_ops.c
+++ node_ops.c
@@ -16,16 +16,13 @@ int _atoi(const char *str)
 		sign = -1;
 		str++;
 	}
-
 	while (*str != '\0')
 	{
 		if (*str < '0' || *str > '9')
 			return (0);
-
 		result = result * 10 + (*str - '0');
 		str++;
 	}
-
 	return (sign * result);
 }
 
